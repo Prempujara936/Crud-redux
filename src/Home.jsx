3880cc8ab26cfecefb7501c36ac4d0c99d1e58e7
krishna2700 <krishnaruparelia0207@gmail.com>
2024-07-24 22:18:39 +0530

    final push

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { deleteUser } from "./UserReducer";
 
 function Home() {
   const users = useSelector((state) => state.users);
-  console.log(users);
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(deleteUser({ id: id }));
+  };
+
   return (
     <div className="container">
       <h2>Crud App with JSON Server</h2>
-      <Link to="/create" className="btn btn-success my-3">Create +</Link>
+      <Link to="/create" className="btn btn-success my-3">
+        Create +
+      </Link>
       <table className="table">
         <thead>
           <tr>
@@ -25,8 +32,18 @@ function Home() {
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>
-                <button className="btn btn-sm btn-primary">Edit</button>
-                <button className="btn btn-sm btn-danger ms-2">Delete</button>
+                <Link
+                  to={`/edit/${user.id}`}
+                  className="btn btn-sm btn-primary"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => handleDelete(user.id)}
+                  className="btn btn-sm btn-danger ms-2"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
