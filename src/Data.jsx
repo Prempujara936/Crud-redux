ea487dfd01ae8b96b438778831139483085bfa71
krishna2700 <krishnaruparelia0207@gmail.com>
2024-07-24 18:00:48 +0530

    first push

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList=[
+{
+    name:'Akshar puajara',
+    email:'aksharpujara369@gmail.com',
+    id:1
+},
+{
+    name:'Krishna Ruparelia',
+    email:'krishnaruparelia0207@gmail.com',
+    id:2
+},
+{
+    name: "Prem pujara",
+    email:'prempujara9@gmail.com',
+    id:3
+}
+]
